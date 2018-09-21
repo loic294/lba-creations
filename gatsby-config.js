f3c664ecb0caf895d43d01f9874a3e731d148170
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2018-09-21 19:26:07 -0400

    Edited manifest colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,10 +8,10 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'gatsby-starter-default',
-        short_name: 'starter',
+        short_name: 'Loïc B.A.',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#217fc9',
+        theme_color: '#217fc9',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
