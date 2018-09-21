ac477842fabd10dd0eaba5d2f7adb2e4d168602c
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2018-09-21 17:45:49 -0400

    Header almost finished

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'LBA Créations',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -17,5 +17,6 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+    `gatsby-plugin-sass`
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
+//import Header from './header'
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -25,18 +25,14 @@ const Layout = ({ children }) => (
             { name: 'description', content: 'Sample' },
             { name: 'keywords', content: 'sample, something' },
           ]}
+          link={[
+            { href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,900', rel: 'stylesheet' }
+          ]}
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+        <div>
           {children}
         </div>
       </>
