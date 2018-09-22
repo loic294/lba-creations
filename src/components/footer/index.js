d8bd1380da7597f9fd7bbe8fb02b5c54a6340ba6
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2018-09-22 09:06:32 -0400

    fixed typo

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,7 @@ import s from './styles.module.scss'
 
 const Footer = () => (
   <footer className={s.footer}>
-    &copy; {new Date().getFullYear()}. Loïc Bellemare-Alford. Tous droits réservers.
+    &copy; {new Date().getFullYear()}. Loïc Bellemare-Alford. Tous droits réservés.
   </footer>
 )
 
