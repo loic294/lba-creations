44d77946679cc753ec181a178680d6d5966aeb49
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2018-09-21 21:00:46 -0400

    Test Netlify forms

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -9,6 +9,20 @@ const Photography = () => (
       <div className={s.email}>
         <a href="mailto:loic@lbacreations.com?subject=Me%20contacter">loic @ lbacreations.com</a>
       </div>
+
+      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact" />
+
+        <input type="text" name="name" placeholder="Nom" />
+        
+        <input type="email" name="email" placeholder="Courriel" />
+        
+        <textarea name="message" placeholder="Message" rows="4"></textarea>
+  
+        <button type="submit">Envoyer</button>
+          
+      </form>
+
     </div>
   </section>
 )
