29948d55ab48fce824f82cf853ca589fc332e278
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2018-09-21 21:12:16 -0400

    test 4

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -10,7 +10,7 @@ const Photography = () => (
         <a href="mailto:loic@lbacreations.com?subject=Me%20contacter">loic @ lbacreations.com</a>
       </div>
 
-      <form name="contact" method="post" action="/" netlify data-netlify="true" data-netlify-honeypot="bot-field">
+      <form name="contact" method="post" action="/" data-netlify="true" data-netlify-honeypot="bot-field">
         <input type="hidden" name="form-name" value="contact" />
 
         <input type="text" name="name" placeholder="Nom" />
