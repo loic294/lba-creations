2b5d9d077f40f338a324b313e6f4aab8a9fbdccd
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2018-09-21 21:38:11 -0400

    Fixed component

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 //import { Link } from 'gatsby'
 import s from './styles.module.scss'
 
@@ -10,47 +10,61 @@ const encode = (data) => {
       .join("&");
 }
 
-const Photography = ({ toastManager }) => (
-  <section id="photography" className={s.container}>
-    <div className="container">
-      <h2>Contacter</h2>
-      <div className={s.email}>
-        <a href="mailto:loic@lbacreations.com?subject=Me%20contacter">loic @ lbacreations.com</a>
-      </div>
+const Photography = ({ toastManager }) => 
 
-      <form netlify onSubmit={e => {
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({ 
-            "form-name": "contact",
-            name: this.name.target.value,
-            email: this.name.target.email,
-            message: this.name.target.message
-          })
-        })
-          .then(() => toastManager.add("Le formulaire a été soumis.", {
-            appearance: 'success',
-            autoDismiss: false,
-          }))
-          .catch(error => alert(error));
-
-        e.preventDefault();
-      }}>
-        <input type="hidden" name="form-name" value="contact" />
-
-        <input type="text" name="name" placeholder="Nom" ref={i => this.name = i} />
-        
-        <input type="email" name="email" placeholder="Courriel" ref={i => this.email = i} />
-        
-        <textarea name="message" placeholder="Message" rows="4"  ref={i => this.message = i}></textarea>
+class Photography extends Component {
   
-        <button type="submit">Envoyer</button>
-          
-      </form>
+  submit = e => {
+    const {toastManager} = this.props
 
-    </div>
-  </section>
-)
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ 
+        "form-name": "contact",
+        name: this.name.target.value,
+        email: this.name.target.email,
+        message: this.name.target.message
+      })
+    })
+      .then(() => toastManager.add("Le formulaire a été soumis.", {
+        appearance: 'success',
+        autoDismiss: false,
+      }))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  }
+
+  render() {
+
+    
+
+    return (
+      <section id="photography" className={s.container}>
+        <div className="container">
+          <h2>Contacter</h2>
+          <div className={s.email}>
+            <a href="mailto:loic@lbacreations.com?subject=Me%20contacter">loic @ lbacreations.com</a>
+          </div>
+    
+          <form netlify onSubmit={this.submit}>
+            <input type="hidden" name="form-name" value="contact" />
+    
+            <input type="text" name="name" placeholder="Nom" ref={i => this.name = i} />
+            
+            <input type="email" name="email" placeholder="Courriel" ref={i => this.email = i} />
+            
+            <textarea name="message" placeholder="Message" rows="4"  ref={i => this.message = i}></textarea>
+      
+            <button type="submit">Envoyer</button>
+              
+          </form>
+    
+        </div>
+      </section>
+    )
+  }
+}
 
 export default withToastManager(Photography)
